aa47e38305f4f9bc27e13e1aaf506f79c6c21a5f
VladislavGorbunov <limitorg2016@yandex.ru>
2025-01-05 22:34:22 +0300

    dfsdffs

--- resources/views/components/search-input.blade.php
+++ resources/views/components/search-input.blade.php
@@ -1,4 +1,4 @@
-<div class="col-12 col-md-7 mt-4">
+<div class="col-12 col-md-6 mt-4 mx-auto">
     
         <div class="input-group">
             <div class="form-control form-control-text text-center">
@@ -7,9 +7,9 @@
         </div>
     
 
-    <!-- <div class="d-flex justify-content-center text-dark mt-4">
+    <div class="d-flex justify-content-center text-dark mt-4">
         <button class="btn-role vacancy" data-role="vacancy">Найти частного мастера</button>
          <i class="bi bi-repeat"></i> 
         <button class="btn-role resume " data-role="resume">Найти сервисный центр</button>
-    </div>    -->
+    </div>   
 </div>
