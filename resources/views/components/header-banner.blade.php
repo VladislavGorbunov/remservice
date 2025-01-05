8e1267c82b28355960d33dfd01a1037a4ff06c82
VladislavGorbunov <limitorg2016@yandex.ru>
2025-01-05 14:12:16 +0300

    5 commit

--- resources/views/components/header-banner.blade.php
+++ resources/views/components/header-banner.blade.php
@@ -1,14 +1,15 @@
 <div class="header-banner d-flex align-items-center justify-content-center">
     <div class="container">
         <div class="row">
-            <div class="col-12 col-md-7">
-                <h1 class="text-center fs-1 mb-2 col-12 col-md-11 mx-auto"><span style="color:#463ee4;">Мастера</span> по ремонту бытовой техники в Москве</h1>
-                <p class="text-center"><strong>ЗаМастером.ру - сервис поиска специалистов по ремонту</strong></p>
-                <p class="text-center">
+            <div class="col-12 col-md-7 left-text-header">
+                <h1 class="mb-2 col-12 col-md-11">Мастера по ремонту бытовой техники в Москве</h1>
+                
+                <p class="">
                     У Вас сломалась бытовая техника в самый не подходящий момент? Вы в поисках опытного мастера по ремонту техники в Москве или области?
                     Наш сервис создан специально для Вас! У нас Вы найдёте опытного и ответственного специалиста который выполнит ремонт быстро, качественно и по доступной цене!
                 </p>
                 <x-search-input/>
+                
             </div>
 
             <div class="col-12 col-sm-5 right-img-header"></div>
--- resources/views/components/search-input.blade.php
+++ resources/views/components/search-input.blade.php
@@ -1,13 +1,13 @@
-<div class="col-12 col-md-8 mx-auto mt-3">
-    <div class="input-group-layer mb-4">
+<div class="col-12 col-md-7 mt-4">
+    
         <div class="input-group">
             <div class="form-control form-control-text text-center">
                 Здравствуйте! Что нужно отремонтировать? <div class="bi-icon-search-block"><i class="bi bi-hand-index-thumb"></i></div>
             </div>
         </div>
-    </div>
+    
 
-    <!-- <div class="d-flex justify-content-center text-dark">
+    <!-- <div class="d-flex justify-content-center text-dark mt-4">
         <button class="btn-role vacancy" data-role="vacancy">Найти частного мастера</button>
          <i class="bi bi-repeat"></i> 
         <button class="btn-role resume " data-role="resume">Найти сервисный центр</button>
