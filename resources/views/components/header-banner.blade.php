8f07f5438bb2c8b816ee62f0328d74c7a7db130a
VladislavGorbunov <75179847+VladislavGorbunov@users.noreply.github.com>
2025-01-05 08:52:54 +0300

    2 commit

--- resources/views/components/header-banner.blade.php
+++ resources/views/components/header-banner.blade.php
@@ -1,4 +1,4 @@
-<div class="header-banner d-flex align-items-center justify-content-center">
+<div class="container"><div class="header-banner d-flex align-items-center justify-content-center">
     <div class="container">
         <div class="row">
             <div class="col-12 col-md-7 p-3">
@@ -7,13 +7,13 @@
                 <p class="text-center">
                     У Вас сломалась бытовая техника в самый не подходящий момент? Вы в поисках опытного мастера по ремонту техники в Москве или области?
                     Наш сервис создан специально для Вас! У нас Вы найдёте опытного и ответственного специалиста который выполнит ремонт быстро, качественно и по доступной цене!
-                </p>ertertggd
+                </p>
                 <x-search-input/>
             </div>
 
             <div class="col-12 col-sm-5 right-img-header"></div>
         </div>   
     </div>
-    <div class="header-layer"></div>
-</div>
+    <!-- <div class="header-layer"></div> -->
+</div></div>
 
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
-<nav class="navbar navbar-dark navbar-expand-lg">
+<nav class="navbar navbar-light navbar-expand-lg fixed-top">
   <div class="container">
-  <a class="navbar-brand" href="#"><i class="bi bi-tools"></i> ЗАМАСТЕРОМ.РУ</a>
+  <a class="navbar-brand" href="#">ЗаМастером</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -9,16 +9,16 @@
     <x-modal-city/>
       <ul class="navbar-nav me-auto text-center py-5 py-md-0">
         <li class="nav-item mt-1 mb-1 mb-lg-0 mt-lg-0 ">
-          <a class="nav-link text-light" href="#">Главная</a>
+          <a class="nav-link" href="#">Главная</a>
         </li>
         <li class="nav-item mt-1 mb-1 mb-lg-0 mt-lg-0 ">
-          <a class="nav-link text-light" href="#">О проекте</a>
+          <a class="nav-link" href="#">О проекте</a>
         </li>
         <li class="nav-item mt-1 mb-1 mb-lg-0 mt-lg-0 ">
-          <a class="nav-link text-light">Контакты</a>
+          <a class="nav-link">Контакты</a>
         </li>
         <li class="nav-item mt-1 mb-1 mb-lg-0 mt-lg-0 ">
-          <a class="nav-link text-light">Реклама</a>
+          <a class="nav-link">Реклама</a>
         </li>
       </ul>
       <form class="d-flex justify-content-center" role="search">
