17d8f7f208113efdce778735496d00dee04e4e74
VladislavGorbunov <limitorg2016@yandex.ru>
2025-01-06 16:49:39 +0300

    отредактирован header

--- resources/views/components/search-input.blade.php
+++ resources/views/components/search-input.blade.php
@@ -1,4 +1,4 @@
-<div class="col-12 col-md-6 mt-4 mx-auto">
+<div class="col-12 col-md-5 mt-4 mx-auto">
     
         <div class="input-group">
             <div class="form-control form-control-text text-center">
