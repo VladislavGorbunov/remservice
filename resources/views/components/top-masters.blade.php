97703de0020c7778d8fad7d453f1d913534cf0de
VladislavGorbunov <limitorg2016@yandex.ru>
2025-01-07 17:14:22 +0300

    изменены стили кнопок при наведении

--- resources/views/components/top-masters.blade.php
+++ resources/views/components/top-masters.blade.php
@@ -2,19 +2,30 @@
     <h2 class="text-center mt-4 mb-4">Топ-10 мастеров Москвы</h2>
     
     <div class="row gx-4">
-        <div class="col-12 col-md-6 mt-2 mb-4">
+        <div class="col-12 col-md-4 mt-2 mb-4">
+            <div class="border p-2 rounded">
+                <div class="row">
+                    <div class="col-12 col-md-7 mx-auto">
+                        <img src="https://sun9-4.userapi.com/6jUPwdybruk1ernfh0nhm1RCu2eSgHNMmDCCbg/rxQ8q3SOoDo.jpg" class="img-fluid">
+                    </div>
+
+                    <div class="col-12 col-md-12 mt-2">
+                        <p class="text-center"><strong>Иванов Виктор Петрович</strong></p>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+
+        <div class="col-12 col-md-4 mt-2 mb-4">
             <div class="border p-3">Мастер</div>
         </div>
 
-        <div class="col-12 col-md-6 mt-2 mb-4">
+        <div class="col-12 col-md-4 mt-2 mb-4">
             <div class="border p-3">Мастер</div>
         </div>
 
-        <div class="col-12 col-md-6 mt-2 mb-4">
-            <div class="border p-3">Мастер</div>
-        </div>
-
-        <div class="col-12 col-md-6 mt-2 mb-4">
+        <div class="col-12 col-md-4 mt-2 mb-4">
             <div class="border p-3">Мастер</div>
         </div>
 
