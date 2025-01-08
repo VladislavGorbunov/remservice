49afe9176a83d5db1cc31f8b228f9d2a9f45784a
VladislavGorbunov <limitorg2016@yandex.ru>
2025-01-08 21:49:25 +0300

    Merge branch 'main' of https://github.com/VladislavGorbunov/remservice

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth')->group(function () {
+Route::middleware('admin')->group(function () {
     Route::get('/admin', [AdminController::class, 'index']);
     
 });
