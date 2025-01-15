fdf4adfcc836241cfcde6cc45c496c242cf62ab1
VladislavGorbunov <75179847+VladislavGorbunov@users.noreply.github.com>
2025-01-15 15:39:29 +0300

    Вёрстка блока топ мастеров

--- resources/views/components/top-masters.blade.php
+++ resources/views/components/top-masters.blade.php
@@ -3,30 +3,92 @@
     
     <div class="row gx-4">
         <div class="col-12 col-md-4 mt-2 mb-4">
-            <div class="border p-2 rounded">
+            <div class="badge-block">
+                <div class="badge-free-exit">Выезд бесплатно</div>
+                <div class="badge-warranty">Даёт гарантию</div>
+            </div>
+            <div class="border p-4 rounded">
+            
                 <div class="row">
                     <div class="col-12 col-md-7 mx-auto">
-                        <img src="https://sun9-4.userapi.com/6jUPwdybruk1ernfh0nhm1RCu2eSgHNMmDCCbg/rxQ8q3SOoDo.jpg" class="img-fluid">
+                        <img src="https://sun9-4.userapi.com/6jUPwdybruk1ernfh0nhm1RCu2eSgHNMmDCCbg/rxQ8q3SOoDo.jpg" class="img-fluid avatar">
                     </div>
 
                     <div class="col-12 col-md-12 mt-2">
-                        <p class="text-center"><strong>Иванов Виктор Петрович</strong></p>
+                        <p class="text-center fs-5 mb-1 mt-2"><strong>Иванов Виктор Петрович</strong></p>
+                        <ul class="list-group list-group-flush">
+                        <li class="list-group-item"><b>Направление:</b> Ремонт стиральных машин</li>
+                        <li class="list-group-item"><b>Возраст:</b> 48 лет</li>
+                        <li class="list-group-item"><b>Опыт ремонта:</b> 18 лет</li>
+                        <li class="list-group-item"><b>Предоставляет гарантию:</b> до 2-х лет</li>
+                        <li class="list-group-item"><b>Образование:</b> Санкт-Петербургский политехнической университет</li>
+                        </ul>
+
+                        <a href="" class="btn btn-get-profile mx-auto mt-3">Перейти к профилю</a>
                     </div>
                 </div>
                 
             </div>
         </div>
 
-        <div class="col-12 col-md-4 mt-2 mb-4">
-            <div class="border p-3">Мастер</div>
-        </div>
+        
+
+
 
         <div class="col-12 col-md-4 mt-2 mb-4">
-            <div class="border p-3">Мастер</div>
+            <div class="badge-block">
+                <div class="badge-warranty">Даёт гарантию</div>
+            </div>
+            <div class="border p-4 rounded">
+                <div class="row">
+                    <div class="col-12 col-md-7 mx-auto">
+                        <img src="https://sun9-4.userapi.com/6jUPwdybruk1ernfh0nhm1RCu2eSgHNMmDCCbg/rxQ8q3SOoDo.jpg" class="img-fluid avatar">
+                    </div>
+
+                    <div class="col-12 col-md-12 mt-2">
+                        <p class="text-center fs-5 mb-1 mt-2"><strong>Иванов Виктор Петрович</strong></p>
+                        <ul class="list-group list-group-flush">
+                        <li class="list-group-item"><b>Направление:</b> Ремонт стиральных машин</li>
+                        <li class="list-group-item"><b>Возраст:</b> 48 лет</li>
+                        <li class="list-group-item"><b>Опыт ремонта:</b> 18 лет</li>
+                        <li class="list-group-item"><b>Предоставляет гарантию:</b> до 2-х лет</li>
+                        <li class="list-group-item"><b>Образование:</b> Санкт-Петербургский политехнической университет</li>
+                        </ul>
+
+                        <a href="" class="btn btn-get-profile mx-auto mt-3">Перейти к профилю</a>
+                    </div>
+                </div>
+                
+            </div>
         </div>
 
+
+
+
+
+
         <div class="col-12 col-md-4 mt-2 mb-4">
-            <div class="border p-3">Мастер</div>
+            <div class="border p-4 rounded">
+                <div class="row">
+                    <div class="col-12 col-md-7 mx-auto">
+                        <img src="https://sun9-4.userapi.com/6jUPwdybruk1ernfh0nhm1RCu2eSgHNMmDCCbg/rxQ8q3SOoDo.jpg" class="img-fluid avatar">
+                    </div>
+
+                    <div class="col-12 col-md-12 mt-2">
+                        <p class="text-center fs-5 mb-1 mt-2"><strong>Иванов Виктор Петрович</strong></p>
+                        <ul class="list-group list-group-flush">
+                        <li class="list-group-item"><b>Направление:</b> Ремонт стиральных машин</li>
+                        <li class="list-group-item"><b>Возраст:</b> 48 лет</li>
+                        <li class="list-group-item"><b>Опыт ремонта:</b> 18 лет</li>
+                        <li class="list-group-item"><b>Предоставляет гарантию:</b> до 2-х лет</li>
+                        <li class="list-group-item"><b>Образование:</b> Санкт-Петербургский политехнической университет</li>
+                        </ul>
+
+                        <a href="" class="btn btn-get-profile mx-auto mt-3">Перейти к профилю</a>
+                    </div>
+                </div>
+                
+            </div>
         </div>
 
         
