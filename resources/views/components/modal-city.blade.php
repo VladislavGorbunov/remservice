400452ca3fa6e39e2ffc4bd090797ef6cacf09cd
VladislavGorbunov <75179847+VladislavGorbunov@users.noreply.github.com>
2025-01-17 13:44:07 +0300

    Добавлен footer

--- resources/views/components/modal-city.blade.php
+++ resources/views/components/modal-city.blade.php
@@ -1,5 +1,5 @@
 <button type="button" class="btn btn-primary btn-modal-city my-3 my-md-0 mx-md-3 d-block mx-auto" data-bs-toggle="modal" data-bs-target="#exampleModal">
-<i class="bi bi-cursor"></i>
+<i class="bi bi-cursor-fill"></i>
 @if ($regionName)
     {{ $regionName }}
 @else
