8cc558c3ccf18598c7f9303e1d126e1bb4771a9d
VladislavGorbunov <75179847+VladislavGorbunov@users.noreply.github.com>
2025-01-24 10:22:00 +0300

    доработка страницы регистрации мастеров

--- resources/views/components/header-banner-short.blade.php
+++ resources/views/components/header-banner-short.blade.php
@@ -6,8 +6,8 @@
             
             </div>
         </div>  
-        <img width="800"  src="https://vsestiralnie.com/upload/iblock/f7a/ru_washer_wf60f1r1f2wdlp_wf60f1r1f2wdlp_001_front_PD_GALLERY_L_JPG_.png" alt="tumble-dryer" class="stir-wash-top"/>
-        <img width="800"  src="https://vsestiralnie.com/upload/iblock/f7a/ru_washer_wf60f1r1f2wdlp_wf60f1r1f2wdlp_001_front_PD_GALLERY_L_JPG_.png" alt="tumble-dryer" class="stir-wash"/>
+        <div class="stir-wash-top"></div>
+        <div class="stir-wash"></div>
     </div>
 </div>
 
--- resources/views/site/master-registration.blade.php
+++ resources/views/site/master-registration.blade.php
@@ -6,32 +6,54 @@
 
     <div class="col-12 col-md-5 d-block mx-auto mt-3">
         
-    <h5 class="text-center">Заполните поля ниже</h5>
+    <h5 class="text-center mt-4">Заполните форму регистрации</h5>
 
     <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">Ваше имя</label>
-        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        <label class="form-label">Ваше имя:</label>
+        <input type="text" class="form-control" placeholder="">
     </div>
 
     <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">Фамилия</label>
-        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        <label class="form-label">Фамилия:</label>
+        <input type="text" class="form-control" placeholder="">
     </div>
 
     <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        <label class="form-label">Электронная почта: (<small>На эту почту будут приходить заявки</small>)</label>
+        <input type="email" class="form-control" placeholder="">
     </div>
 
     <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        <label class="form-label">Номер телефона: (<small>На этот номер будут приходить заявки</small>)</label>
+        <input type="text" class="form-control" placeholder="">
     </div>
 
     <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        <label class="form-label">Опыт ремонта:</label>
+        <select class="form-select" aria-label="Default select example">
+            <option selected>1 год</option>
+            <option value="1">2 года</option>
+            <option value="2">3 года</option>
+            <option value="3">4 года</option>
+            <option value="3">Более 5 лет</option>
+            <option value="3">Более 10 лет</option>
+            <option value="3">Более 15 лет</option>
+            <option value="3">Более 20 лет</option>
+        </select>
     </div>
+
+    
+    <div class="mb-3">
+        <label class="form-label">Город:</label>
+        <select class="form-select" aria-label="Default select example">
+            <option selected>Москва</option>
+            <option value="1">Санкт-Петербург</option>
+        </select>
+    </div>
+
+    
+
+    
 
     </div>
     <x-footer/>
