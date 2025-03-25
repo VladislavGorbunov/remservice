bdd096eb188b9c67f2d59566a4e4071d064eaf2a
Vladislav <limitorg2016@ya.ru>
2025-03-25 19:32:44 +0300

    изменение дизайна

--- resources/views/components/search-input.blade.php
+++ resources/views/components/search-input.blade.php
@@ -1,7 +1,7 @@
 <div class="col-12 col-md-5 mt-4 mx-auto">
         <div class="input-group">
             <div class="form-control form-control-text text-center">
-                Здравствуйте! Что нужно отремонтировать? <div class="bi-icon-search-block"><i class="bi bi-hand-index-thumb"></i></div>
+                Здравствуйте! Что нужно отремонтировать?
             </div>
         </div>
 </div>
